bbd4a6232c3533a41a977a5a2f5f9d3c7d827613
wekesae154 <wekesae154@strose.edu>
2015-03-06 06:28:44 -0500

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,8 +1,8 @@
 class hello {
 public static void main(String[] args)
 {
-	//prints out the names of the team members
+	
 	System.out.println("Manasa and Ephemiah");
-	System.out.println("Hello world");
+
 }
 }
